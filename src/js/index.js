fd742af4db55ffa94f96e07cd346b60dbf52631b
uday <udaygopal07@gmail.com>
2023-11-07 14:32:48 +0530

    code  modified

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
 /**
  *this will contain js logic
  */
+ function addProduct() {
+    const productList = document.getElementById('productList');
+    const row = productList.insertRow();
+    row.innerHTML = `
+      <td><input type="text" placeholder="Product Name"></td>
+      <td><input type="number" placeholder="Price"></td>
+    `;
+  }
+  
+  function calculateTax() {
+    const rows = document.getElementById('productList').getElementsByTagName('tr');
+    let totalTax = 0;
+  
+    for (let i = 0; i < rows.length; i++) {
+      const price = parseFloat(rows[i].querySelector('input[type="number"]').value);
+      const tax = (price * 0.15).toFixed(2); // Assuming a tax rate of 15%
+  
+      totalTax += isNaN(tax) ? 0 : parseFloat(tax);
+    }
+  
+    document.getElementById('totalTax').innerText = `₹${totalTax}`;
+  }
